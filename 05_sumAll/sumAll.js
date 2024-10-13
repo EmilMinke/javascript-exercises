7b2d034227b7e4736ce633f1e9c2d76beb196325
EmilMinke <em.minke@gmail.com>
2024-10-13 14:40:26 +0200

    feat: modify sumAll to fit testcases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
+  const isPositiveInteger = (positiveInt) => {
+    return (
+      typeof positiveInt === "number" &&
+      Number.isInteger(positiveInt) &&
+      positiveInt > 0
+    );
+  };
 
+  if (!isPositiveInteger(firstInt) || !isPositiveInteger(secondInt)) {
+    return "ERROR";
+  }
+
+  const start = Math.min(firstInt, secondInt);
+  const end = Math.max(firstInt, secondInt);
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
